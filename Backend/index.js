c69ebb00c1faac7043d1df4219ae5aab68d09999
Revlis15 <bopbin15@gmail.com>
2024-12-10 22:42:04 +0700

    update register

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://project-1-frontend-sigma.vercel.app'],
     credentials: true
 }));
 
